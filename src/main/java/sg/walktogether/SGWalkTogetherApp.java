b58d3fb4514ad33cd84a364c964999f3721263a2
Isak Rabin <isak_r@yahoo.com>
2024-05-25 20:20:37 +0800

    Init Project

---
+++ src/main/java/sg/walktogether/SGWalkTogetherApp.java
@@ -0,0 +1,13 @@
+package sg.walktogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SGWalkTogetherApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SGWalkTogetherApp.class, args);
+	}
+
+}
---
+++ src/test/java/sg/walktogether/SgGoAroundApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.walktogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgGoAroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
